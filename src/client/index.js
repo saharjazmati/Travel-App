8825689f4332ab2344b88cea4741b7d0a4512a61
saharjazmati <75436423+saharjazmati@users.noreply.github.com>
2021-01-09 19:48:30 +0200

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,21 @@
+import {
+    performAction,
+    getFromGeoN,
+    getFromW_Bit,
+    getFromPbay,
+    updateUI,
+    postData,
+  } from "./js/app.js";
+  
+  import "./styles/style.scss";
+  import "./styles/header.scss";
+  import "./styles/output.scss";
+  
+  export {
+    performAction,
+    getFromGeoN,
+    getFromW_Bit,
+    getFromPbay,
+    updateUI,
+    postData,
+  };
