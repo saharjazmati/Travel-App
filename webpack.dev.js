75e44623831be647a946ca3ab5af35e8b15a1adc
saharjazmati <75436423+saharjazmati@users.noreply.github.com>
2021-01-09 19:45:41 +0200

    Add files via upload

---
+++ webpack.dev.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const sass = require("node-sass");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+module.exports = {
+  entry: "./src/client/index.js",
+  mode: "development",
+  devtool: "source-map",
+  devServer: {
+    port: 3000,
+  },
+  output: {
+    libraryTarget: "var",
+    library: "Client",
+  },
+  module: {
+    rules: [
+      {
+        test: "/.js$/",
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+    new WorkboxPlugin.GenerateSW(),
+  ],
+};
