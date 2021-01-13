e4034a8fe07b425987dce987a90a57ae8c3ef9c5
saharjazmati <75436423+saharjazmati@users.noreply.github.com>
2021-01-13 17:37:10 +0200

    Update app.js

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -3,20 +3,50 @@ const GeoNames_user = "sisisasa";
 const weatherBitKpi = "37312b9b3eac4ba3a484c04a78ae844c";
 const pixabayKey = "19823186-6d0fc87f33685ba4372bd9385";
 
+const getFromGeoN = async (city) => {
+    const url = `http://api.geonames.org/searchJSON?q=${city}&maxRows=1&username=${GeoNames_user}`;
+    const res = await fetch(url);
+    try {
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      alert("GeoNames error!", error);
+    }
+  };
+  
+  const getFromW_Bit = async (lat, lng) => {
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lng}&key=${weatherBitKpi}`;
+    const res = await fetch(url);
+    try {
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      alert("WeatherBit error!", error);
+    }
+  };
+  
+  const getFromPbay = async (city) => {
+    const url = `https://pixabay.com/api/?key=${pixabayKey}&q=${city}&image_type=photo`;
+    const res = await fetch(url);
+    try {
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      alert("Pixabay error!", error);
+    }
+  };
+  
 const performAction = async () => {
-  // getting city and dates
+  // save city and dates values in variables 
   const city = document.getElementById("city").value;
   const departureDate = document.getElementsByClassName("dates")[0].value;
   const returnDate = document.getElementsByClassName("dates")[1].value;
-
-  // Countdown and length of trip
+  // Calculations : Trip length and days till traveling
   let NewDate = new Date();
-  const daysToLeave = Math.floor(
-    (new Date(departureDate).getTime() - NewDate.getTime()) / (1000 * 3600 * 24)
-  );
-  const TripLength = Math.ceil(
-    (new Date(returnDate).getTime() - new Date(departureDate).getTime())/(1000 * 3600 * 24));
-  document.getElementById("tripInfo").innerHTML = `The trip is in ${daysToLeave} days from now <br> You'll be traveling for ${TripLength} days`;
+  const daysToLeave = Math.floor((new Date(departureDate).getTime() - NewDate.getTime())/(1000 * 3600 * 24));
+  const TripLength = Math.ceil((new Date(returnDate).getTime() - new Date(departureDate).getTime())/(1000 * 3600 * 24));
+  const tripInformation = `${city} trip is in ${daysToLeave} days from now <br> You'll be traveling for ${TripLength} days`;
+  document.getElementById("tripInfo").innerHTML = tripInformation;
   getFromGeoN(city)
     .then((data) => {
       return postData("http://localhost:3030/geonames", {
@@ -49,52 +79,12 @@ const performAction = async () => {
     });
 };
 
-const getFromGeoN = async (city) => {
-  const url = `http://api.geonames.org/searchJSON?q=${city}&maxRows=1&username=${GeoNames_user}`;
-  const res = await fetch(url);
-  try {
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    alert("GeoNames error!", error);
-  }
-};
-
-const getFromW_Bit = async (lat, lng) => {
-  const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lng}&key=${weatherBitKpi}`;
-  const res = await fetch(url);
-  try {
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    alert("WeatherBit error!", error);
-  }
-};
-
-const getFromPbay = async (city) => {
-  const url = `https://pixabay.com/api/?key=${pixabayKey}&q=${city}&image_type=photo`;
-  const res = await fetch(url);
-  try {
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    alert("Pixabay error!", error);
-  }
-};
 
 const updateUI = async () => {
   const res = await fetch("http://localhost:3030/data");
-
   try {
     const allData = await res.json();
-    document.getElementById(
-      "details"
-    ).innerHTML = `Weather Forecast <br> High: ${
-      allData[allData.length - 2].high
-    }, Low: ${allData[allData.length - 2].low} <br>  ${
-      allData[allData.length - 2].description
-    }`;
-
+    document.getElementById("details").innerHTML = `Weather Forecast <br> High: ${allData[allData.length - 2].high}, Low: ${allData[allData.length - 2].low} <br> ${allData[allData.length - 2].description}`;
     document.getElementById("image").src = allData[allData.length - 1].image;
   } catch (error) {
     console.log(error);
@@ -105,9 +95,7 @@ const postData = async (url = "", data = {}) => {
   const response = await fetch(url, {
     method: "POST",
     credentials: "same-origin",
-    headers: {
-      "Content-Type": "application/json",
-    },
+    headers: {"Content-Type": "application/json",},
     body: JSON.stringify(data),
   });
   try {
@@ -124,10 +112,10 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 export {
-  performAction,
-  getFromGeoN,
-  getFromW_Bit,
-  getFromPbay,
-  updateUI,
-  postData,
+    getFromGeoN,
+    getFromW_Bit,
+    getFromPbay,
+    performAction,
+    updateUI,
+    postData,
 };
